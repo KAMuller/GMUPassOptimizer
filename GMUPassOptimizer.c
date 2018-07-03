b014f7671a3bd2ca10d71d13a036172857455644
Saru Kalva <sarukalva@masonlive.gmu.edu>
2018-07-03 16:02:29 -0400

    added building names with codes (instead of w/ numbers)

--- GMUPassOptimizer.c
+++ GMUPassOptimizer.c
@@ -4,7 +4,56 @@
 //Saru Kalva
 void PrintBuildings()
 {
-	printf("1: Johnson Center\n");
+	printf("AB: Art and Design Building\n");
+	printf("AFC: Aquatic and Fitness Center\n");
+	printf("AQ: Aquia Building\n");
+	printf("BL: Blue Ridge Hall\n");
+	printf("BUCHAN: Buchanan Hall\n");
+	printf("CAROW: Carow Hall\n");
+	printf("CFA: Center for the Arts\n");
+	printf("CH: College Hall\n");
+	printf("DK: David J. King Hall\n");
+	printf("E: East Building\n");
+	printf("ENGR: Nguyen Engineering Building\n");
+	printf("ENT: Enterprise Hall\n");
+	printf("ESNHWR: Eisenhower\n");
+	printf("ESTSHR: Eastern Shore\n");
+	printf("EXPL: Exploratory Hall\n");
+	printf("FENWCK: Fenwick Library\n");
+	printf("FH: Field House\n");
+	printf("FIELD: Athletic Fields located on the West Campus next to the Field House\n");
+	printf("FINLEY: Finley Building\n");
+	printf("GLOBAL: Mason Global Center\n");
+	printf("HNOVR: Hanover Hall\n");
+	printf("HR: Hampton Roads\n");
+	printf("HUB: The Hub\n");
+	printf("IN: Innovation Hall\n");
+	printf("HR: Hampton Roads\n");
+	printf("JC: Johnson Center\n");
+	printf("KA: Keller Annex\n");
+	printf("KAII: Keller Annex II\n");
+	printf("KB: Krasnow Building\n");
+	printf("KH: Krug Hall\n");
+	printf("LH: Lecture Hall\n");
+	printf("MERTEN: Merten Hall\n");
+	printf("MH: Mason Hall\n");
+	printf("MIH: Mason Inn Hotel and Conference Center\n");
+	printf("MTB: Music Theater Building\n");
+	printf("NEM: North East Module\n");
+	printf("PAB: de Laski Performing Arts Building\n");
+	printf("PETRSN: Peterson Hall\n");
+	printf("PIEDMT: Piedmont Hall\n");
+	printf("PILOT: Pilot House\n");
+	printf("PLANET: Planetary Hall\n");
+	printf("R: Robinson Hall\n");
+	printf("RAC: Recreation Athletic Complex\n");
+	printf("ROGER: Roger Hall\n");
+	printf("RSCH: Research Hall\n");
+	printf("SNDBRGE: Sandbridge Hall\n");
+	printf("SUBI: Student Union I\n");
+	printf("T: Thompson Hall\n");
+	printf("W: West Building\n");
+
 }
 
 int PickBuilding(int day, int time)
@@ -15,71 +64,71 @@ int PickBuilding(int day, int time)
 		case 0;
 			if (time == 0)
 			{
-				printf("choose the building of your first monday class\n");
+				printf("Enter the building of your first monday class\n");
 			}
 			else
 			{
-				printf("choose the building of your last monday class\n");
+				printf("Enter the building of your last monday class\n");
 			}
 		break;
 		case 1:
 			if (time == 0)
 			{
-				printf("choose the building of your first tuesday class\n");
+				printf("Enter the building of your first tuesday class\n");
 			}
 			else
 			{
-				printf("choose the building of your last tuesday class\n");
+				printf("Enter the building of your last tuesday class\n");
 			}
 		break;
 		case 2:
 			if (time == 0)
 			{
-				printf("choose the building of your first wednesday class\n");
+				printf("Enter the building of your first wednesday class\n");
 			}
 			else
 			{
-				printf("choose the building of your last wednesday class\n");
+				printf("Enter the building of your last wednesday class\n");
 			}
 		break;
 		case 3:
 			if (time == 0)
 			{
-				printf("choose the building of your first thursday class\n");
+				printf("Enter the building of your first thursday class\n");
 			}
 			else
 			{
-				printf("choose the building of your last thursday class\n");
+				printf("Enter the building of your last thursday class\n");
 			}
 		break;
 		case 4:
 			if (time == 0)
 			{
-				printf("choose the building of your first friday class\n");
+				printf("Enter the building of your first friday class\n");
 			}
 			else
 			{
-				printf("choose the building of your last friday class\n");
+				printf("Enter the building of your last friday class\n");
 			}
 		break;
 		case 5:
 			if (time == 0)
 			{
-				printf("choose the building of your first saturday class\n");
+				printf("Enter the building of your first saturday class\n");
 			}
 			else
 			{
-				printf("choose the building of your last saturday class\n");
+				printf("Enter the building of your last saturday class\n");
 			}
 		break;
 		case 6:
 			if (time == 0)
 			{
-				printf("choose the building of your first sunday class\n");
+				printf("Enter the building of your first sunday class\n");
 			}
 			else
 			{
-				printf("choose the building of your last sunday class\n");
+				printf("Enter the building of your last sunday class\n");
 			}
 		break;
 		default:
