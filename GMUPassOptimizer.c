e8004a015cbf1982134687d83cfe9909643b9ba7
Saru Kalva <sarukalva@masonlive.gmu.edu>
2018-07-11 15:56:08 -0400

    heap plus doubts

--- GMUPassOptimizer.c
+++ GMUPassOptimizer.c
@@ -6,20 +6,21 @@
 
 
 typedef struct AdjNode_ {
-	int index;
-	int weight;
-	struct AdjNode_* next;
+	int index; //represents vertex
+	int weight; //represents edge distance
+	struct AdjNode_* next; //linked list
 } AdjNode;
 
 typedef struct AdjList_ {
-	struct AdjNode* head;
+	struct AdjNode* head; //to access list
 } AdjList;
 
 typedef struct Graph_ {
-	int V;
-	struct AdjList* array;
+	int V; //number of vertices
+	struct AdjList* array; //array of llists
 } Graph;
 
+//adds an adjacent node to vertex of specified index
 AdjNode* newAdjNode(int index, int weight)
 {
 	AdjNode* newNode = malloc(sizeof(AdjNode));//add malloc check
@@ -29,6 +30,7 @@ AdjNode* newAdjNode(int index, int weight)
 	return newNode;
 }
 
+//confusing
 Graph* createGraph(int V)
 {
 	Graph* graph = malloc(sizeof(Graph));//add malloc check
@@ -42,7 +44,8 @@ Graph* createGraph(int V)
 	return graph;
 }
 
-void AddEdge(Graph graph, int src, int dest, int weight)
+
+void AddEdge(Graph graph, /*?*/int src, int dest, int weight)
 {
 	AdjNode* newNode = newAdjNode(dest, weight);
 	newNode->next = graph->array[src].head;
@@ -52,6 +55,151 @@ void AddEdge(Graph graph, int src, int dest, int weight)
     newNode->next = graph->array[dest].head;
     graph->array[dest].head = newNode;
 }
+
+struct MinHeapNode{
+	int v; //index in array
+	int dist; //shortest path
+}
+
+struct MinHeap{
+	int size;
+	int capacity;
+	int *pos;//location of ?
+	struct MinHeapNode **array; //why two **?
+}
+
+/////
+struct MinHeapNode* newMinHeapNode(int v, int dist)
+{
+    struct MinHeapNode* minHeapNode =
+           (struct MinHeapNode*) malloc(sizeof(struct MinHeapNode));
+    minHeapNode->v = v;
+    minHeapNode->dist = dist;
+    return minHeapNode;
+}
+
+/////
+struct MinHeap* createMinHeap(int capacity)
+{
+    struct MinHeap* minHeap =
+         (struct MinHeap*) malloc(sizeof(struct MinHeap));
+    minHeap->pos = (int *)malloc(capacity * sizeof(int));
+    minHeap->size = 0;
+    minHeap->capacity = capacity;
+    minHeap->array =
+         (struct MinHeapNode**) malloc(capacity * sizeof(struct MinHeapNode*));
+    return minHeap;
+}
+
+
+void swapMinHeapNode(struct MinHeapNode** a, struct MinHeapNode** b)
+{
+    struct MinHeapNode* t = *a;
+    *a = *b;
+    *b = t;
+}
+
+//percolate DOWN
+void minHeapify(struct MinHeap* minHeap, int idx)
+{
+    int smallest, left, right;
+    smallest = idx;
+    left = 2 * idx + 1;
+    right = 2 * idx + 2;
+ 
+    if (left < minHeap->size &&
+        minHeap->array[left]->dist < minHeap->array[smallest]->dist )
+      smallest = left;
+ 
+    if (right < minHeap->size &&
+        minHeap->array[right]->dist < minHeap->array[smallest]->dist )
+      smallest = right;
+ 
+    if (smallest != idx)
+    {
+        // The nodes to be swapped in min heap
+        MinHeapNode *smallestNode = minHeap->array[smallest];
+        MinHeapNode *idxNode = minHeap->array[idx];
+ 
+        // Swap positions
+        minHeap->pos[smallestNode->v] = idx;
+        minHeap->pos[idxNode->v] = smallest;
+ 
+        // Swap nodes
+        swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]);
+ 
+        minHeapify(minHeap, smallest);
+    }
+}
+
+int isEmpty(struct MinHeap* minHeap)
+{
+    return minHeap->size == 0;
+}
+
+//delete top and percolate top node DOWN
+struct MinHeapNode* extractMin(struct MinHeap* minHeap)
+{
+    if (isEmpty(minHeap))
+        return NULL;
+ 
+    // Store the root node
+    struct MinHeapNode* root = minHeap->array[0];
+ 
+    // Replace root node with last node
+    struct MinHeapNode* lastNode = minHeap->array[minHeap->size - 1];
+    minHeap->array[0] = lastNode;
+ 
+    // Update position of last node
+    minHeap->pos[root->v] = minHeap->size-1;
+    minHeap->pos[lastNode->v] = 0;
+ 
+    // Reduce heap size and heapify root
+    --minHeap->size;
+    minHeapify(minHeap, 0);
+ 
+    return root;
+}
+
+//PERCOLATE UP and update dist val
+void decreaseKey(struct MinHeap* minHeap, int v, int dist)
+{
+    // Get the index of v in  heap array
+    int i = minHeap->pos[v];
+ 
+    // Get the node and update its dist value
+    minHeap->array[i]->dist = dist;
+ 
+    // Travel up while the complete tree is not heapified.
+    // This is a O(Logn) loop
+    while (i && minHeap->array[i]->dist < minHeap->array[(i - 1) / 2]->dist)
+    {
+        // Swap this node with its parent
+        minHeap->pos[minHeap->array[i]->v] = (i-1)/2;
+        minHeap->pos[minHeap->array[(i-1)/2]->v] = i;
+        swapMinHeapNode(&minHeap->array[i],  &minHeap->array[(i - 1) / 2]);
+ 
+        // move to parent index
+        i = (i - 1) / 2;
+    }
+}
+
+//if the vertex is in heap
+bool isInMinHeap(struct MinHeap *minHeap, int v)
+{
+   if (minHeap->pos[v]/*why?*/ < minHeap->size)
+     return true;
+   return false;
+}
+ 
+/*// A utility function used to print the solution
+void printArr(int dist[], int n)
+{
+    printf("Vertex   Distance from Source\n");
+    for (int i = 0; i < n; ++i)
+        printf("%d \t\t %d\n", i, dist[i]);
+}*/
+
 
 
 void PrintBuildings()
